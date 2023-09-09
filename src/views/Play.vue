4955c0cfd5a8861c514ae1ad2be0960c3f8cb6ff
Tomasz Piwowski <saysaeqo@gmail.com>
2023-09-09 03:33:28 +0200

    demo panelu gracza

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="section">
     <div class="player_main">
-      <img src="@/assets/standing_human.svg">
+      <div class="player_model">
+        <table>
+          <tr><td></td><td></td></tr>
+          <tr><td></td><td></td></tr>
+          <tr><td></td><td></td></tr>
+        </table>
+      </div>
       <div class="player_info">
         <img src="@/assets/user_icon.svg">
-        <span>Nazwa:</span>
-        <span>Życie/mana:</span>
-        <span>Przedmioty:</span>
+        <span><b>Nazwa:</b> Mahiro Suda</span>
+        <span><b>Zdrowie:</b> 100%</span>
+        <span><b>Mana:</b> 35 000 / 50 000</span>
+        <span style="flex: 3; align-items: flex-start; padding-top: 1.5em; box-sizing: border-box; flex-direction: column;">
+          <b>Przedmioty:</b>
+          <table style="margin: 0.5em;">
+            <tr><td></td><td></td><td></td><td></td><td></td></tr>
+            <tr><td></td><td></td><td></td><td></td><td></td></tr>
+            <tr><td></td><td></td><td></td><td></td><td></td></tr>
+          </table>
+        </span>
       </div>
     </div>
     <span>Aktualna lokacja</span>
@@ -38,11 +52,35 @@ export default {
   justify-content: center;
   gap: 1em;
 
-  > img{
-  height: 20em;
-  border: 0.2em solid $color_darker;
-  padding: 1em;
-}
+  .player_model{
+    border: 0.2em solid $color_darker;
+    box-sizing: border-box;
+    width: 50%;
+    background-image: url("../assets/standing_human.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-origin: content-box;
+    display: flex;
+
+    table {
+      display: flex;
+      justify-content: start;
+      align-items: center;
+      flex-direction: column;
+      flex: 1;
+    }
+    tr {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 4.5em;
+    }
+    td {
+      margin: 0 3.5em 0 3.5em;
+      opacity: 0.9;
+      padding: 2em;
+    }
+  }
 }
 
 
@@ -57,16 +95,27 @@ export default {
   img {
     border: 0.2em solid $color_darker;
     border-radius: 100em;
-    height: 6.5em;
+    width: 50%;
+    box-sizing: border-box;
+    align-self: center;
   }
 
   span {
     height: auto;
     flex: 1;
     align-self: stretch;
+    justify-content: flex-start;
     text-align: start;
     text-indent: 1em;
+    margin: 0 1em 0 1;
   }
 }
+
+td {
+    border: 0.2em solid $color_darker;
+    padding: 1.8em;
+    box-sizing: border-box;
+    background-color: $color_darker;
+  }
 
 </style>
